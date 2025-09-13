e597b892ba9ec10a49d44797ef9a2e6b6600c720
Mujeeb <lawalsakamujeeb@gmail.com>
2025-09-13 12:08:47 -0400

    feat: Define variables for VPC configuration including CIDR, public and private subnets, and availability zones

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type = list(string)
+}
